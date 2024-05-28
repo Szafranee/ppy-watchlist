9306b6f4322feb10db8e11822e008660a16e0f9a
Kacper Szafrański <kacperr.szafranski@gmail.com>
2024-05-28 19:28:09 +0200

    Add new exception

--- exceptions.py
+++ exceptions.py
@@ -1,5 +1,14 @@
 class ScaleError(Exception):
     """Exception raised when the value is not in the scale"""
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+class MovieNotFound(Exception):
+    """Exception raised when the movie is not found in the collection"""
+
     def __init__(self, message):
         self.message = message
         super().__init__(self.message)
